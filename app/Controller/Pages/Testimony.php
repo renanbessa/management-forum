c3ac1e05b9e6a77a21e8956c06164db74233cc19
Renan Bessa <renan.bessa@idinheiro.com.br>
2022-08-08 22:56:51 -0300

    feat: insert controller pagination

--- app/Controller/Pages/Testimony.php
+++ app/Controller/Pages/Testimony.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Pages;
 
+use WilliamCosta\DatabaseManager\Pagination;
 use \App\Model\Entity\Testimony as EntityTestimony;
 use \App\Utils\View;
 
@@ -10,15 +11,26 @@ class Testimony extends Page
     /**
      * Método responsável por obter a renderização dos itens de depoimentos para a página
      *
+     * @param Request $request
      * @return string
      */
-    private static function getTestimonyItems()
+    private static function getTestimonyItems($request)
     {
         //DEPOIMENTOS
         $itens = '';
 
+        //QUANTIDADE TOTAL DE REGISTROS
+        $quantidadeTotal = EntityTestimony::getTestimonies(null, null, null, 'COUNT(*) as qtd')->fetchObject()->qtd;
+
+        //PÁGINA ATUAL
+        $queryParams = $request->getQueryParams();
+        $paginaAtual = $queryParams['page'] ?? 1;
+
+        //INSTÂNCIA DE PAGINAÇÃO
+        $obPagination = new Pagination($quantidadeTotal, $paginaAtual, 3);
+
         //RESULTADOS DA PÁGINA
-        $results = EntityTestimony::getTestimonies(null, 'id DESC');
+        $results = EntityTestimony::getTestimonies(null, 'id DESC', $obPagination->getLimit());
 
         //RENDERIZA O ITEM
         while ($obTestimony = $results->fetchObject(EntityTestimony::class)) {
@@ -36,14 +48,15 @@ class Testimony extends Page
     /**
      * Método responsável por retornar o conteúdo (view) de depoimentos
      *
+     * @param Request $request
      * @return string
      */
-    public static function getTestimonies()
+    public static function getTestimonies($request)
     {
 
         // VIEW DE DEPOIMENTOS
         $content = View::render('pages/testimonies', [
-            'itens' => self::getTestimonyItems(),
+            'itens' => self::getTestimonyItems($request),
         ]);
 
         // RETORNA A VIEW DA PÁGINA
@@ -67,6 +80,7 @@ class Testimony extends Page
         $obTestimony->mensagem = $postVars['mensagem'];
         $obTestimony->cadastrar();
 
-        return self::getTestimonies();
+        //RETORNA A PÁGINA DE LISTAGEM DE DEPOIMENTOS
+        return self::getTestimonies($request);
     }
 }
